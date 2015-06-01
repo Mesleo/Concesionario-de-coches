55df4620d5f05696e24d393f19173895aedfab2a
JaviBP <javifocus88@hotmail.com>
2015-06-01 18:35:30 +0200

    Concesionario con ficheros y excepciones

---
+++ src/Marca.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package pgn.examenMarzo.concesionarioCoches;
+
+/**
+ * Representa cada una de las marcas
+ * 
+ * @author Javier Benítez del Pozo
+ * @version 2.0
+ */
+public enum Marca {
+	
+	/**
+	 * Representa la marca BMW
+	 */
+	BMW, 
+	
+	/**
+	 * Representa la marca SEAT
+	 */
+	SEAT
+	
+}
